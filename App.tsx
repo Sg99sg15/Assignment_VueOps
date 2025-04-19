525937754ce38c3e68ca5646558eb1016718908c
shashankAkos <shashank.gupta@akosmdtech.com>
2025-04-19 23:00:55 +0530

    hide warnings

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, LogBox } from 'react-native'
+import React, { useEffect } from 'react'
 import AppNavigator from './src/navigations/AppNavigator'
 import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => {
+  useEffect(() => {
+    LogBox.ignoreAllLogs(true);
+  }, [])
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }}>
